ecae60db844b7b2ee8fec57a893a0ab34f5b310c
Bradley Stachurski <bradley.stachurski@gmail.com>
2016-03-20 23:12:28 -0500

    Starting with transitions

--- src/js/main.js
+++ src/js/main.js
@@ -18,12 +18,25 @@ var arc = d3.svg.arc()
     .startAngle(.65 * Math.PI)
     .endAngle(2.35 * Math.PI);
 
+var innerArc = d3.svg.arc()
+    .innerRadius(0)
+    .outerRadius(donutWidth)
+    .startAngle(0 * Math.PI)
+    .endAngle(1.5 * Math.PI);
+
 svg.append('path')
     .attr('d', arc)
     .attr('fill', 'rgb(194, 27, 43)');
 
-svg.append('circle')
+/*svg.append('circle')
     .attr('cx', 0)
     .attr('cy', 0)
     .attr('r', 90)
-    .attr('fill', 'rgb(255, 217, 0)');
+    .attr('fill', 'rgb(255, 235, 102)');*/
+
+var countdown = svg.append('path')
+    .attr('d', innerArc)
+    .attr('fill', 'rgb(255, 217, 0');
+
+
+//Todo: Add transition with timer associated with countdown
